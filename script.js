c8fc0ef6a7d01987f4d3d7c5df94bc85bdf9768c
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-12 16:35:23 -0600

    I got a red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Your JS goes here
+var body = document.getElementsByTagName('body')[0];
+
+for (let i=1; i<63; i++){
+  if (i%2===0){
+    var redTile = document.createElement('div');
+    redTile.style.backgroundColor = 'red';
+    redTile.style.width = '11.1%';
+    redTile.style.float = 'left';
+    redTile.style.paddingBottom = '11.1%'
+    body.appendChild(redTile);
+  } else {
+    var blackTile = document.createElement('div');
+    blackTile.style.backgroundColor = 'black';
+    blackTile.style.width = '11.1%';
+    blackTile.style.float = 'left';
+    blackTile.style.paddingBottom = '11.1%'
+    body.appendChild(blackTile);
+  }
+}
