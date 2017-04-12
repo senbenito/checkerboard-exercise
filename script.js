25325f02625c28db17a0028fc8160a3d4c98cd49
Shannon Rivers <shannon.rivers@gmail.com>
2017-04-12 17:16:21 -0600

    made a random color checkerboard

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 var body = document.getElementsByTagName('body')[0];
 
+function getRandomIntInclusive(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 for (let i=1; i<64; i++){
-  if (i%2===0){
-    var redTile = document.createElement('div');
-    redTile.style.backgroundColor = 'red';
-    redTile.style.width = '11.1%';
-    redTile.style.float = 'left';
-    redTile.style.paddingBottom = '11.1%'
-    body.appendChild(redTile);
-  } else {
-    var blackTile = document.createElement('div');
-    blackTile.style.backgroundColor = 'black';
-    blackTile.style.width = '11.1%';
-    blackTile.style.float = 'left';
-    blackTile.style.paddingBottom = '11.1%'
-    body.appendChild(blackTile);
-  }
+    var tile = document.createElement('div');
+    tile.style.backgroundColor = `rgb(${getRandomIntInclusive(0,255)}, ${getRandomIntInclusive(0,255)}, ${getRandomIntInclusive(0,255)})`;
+    tile.style.width = '11.1%';
+    tile.style.float = 'left';
+    tile.style.paddingBottom = '11.1%';
+    body.appendChild(tile);
 }
